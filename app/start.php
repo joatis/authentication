7ef418960743252abb7b1a7017d4a1af1235a413
joatis <joatis@joatis3.com>
2015-06-20 10:06:48 -0400

    Adding DataBase Support (6/30)
    Adding Database support required adding a new file database.php that is
    then required by start.php.
    
    database.php loads the Illuminate\Database\Capsule\Manager and aliases it
    to just 'Capsule'. A new instance of Capsule is created and it's
    properties are loaded via $app->config->get('xxx').
    
    Capsule's bootEloquent method is called which should give us a connection
    to the database and an interface to the user model.

--- app/start.php
+++ app/start.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\Slim;
+use Noodlehaus\Config;
 
 session_cache_limiter(false);
 session_start();
@@ -12,8 +13,22 @@ define('INC_ROOT', dirname(__DIR__));
 
 require INC_ROOT . '/vendor/autoload.php';
 
-$app = new Slim();
+$app = new Slim([
+  // Some editors will append a newline so that's why the rtrim is here.
+  'mode' => rtrim(file_get_contents(INC_ROOT . '/mode.php'))
+]);
 
-$app->get('/test/:name', function($name){
-  echo "Hello {$name}";
+
+/*
+How to access configuration options
+$app->config->get('db.driver');
+*/
+$app->configureMode($app->config('mode'), function() use ($app) {
+  $app->config = Config::load(INC_ROOT . "/app/config/{$app->mode}.php");
 });
+
+/*
+Require the database.php file because that is the file
+that will allow us to use Laravel's Eloquent Data models.
+*/
+require 'database.php';
