9defa9663a4957973c100ea5b4ca99ac92eddeb8
joatis <joatis@joatis3.com>
2015-07-18 19:38:31 -0400

    Sending Email 15/30
    Added capability to send an email when a user successfully registers.
    Sending HTML email from a view and passing data via appendData so it can
    be used in the template.
    The mailer is set via configuration and in this instance it's PHPMAiler
    The new Mailer class creates an instance for a particular Mailer and view.
    The new Message class handles methods to, subject and body.

---
+++ app/Joatis3/Mail/Message.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Joatis3\Mail;
+
+class Message
+{
+  protected $mailer;
+
+  public function __construct($mailer)
+  {
+    $this->mailer = $mailer;
+  }
+
+  public function to($address)
+  {
+    $this->mailer->addAddress($address);
+  }
+
+  public function subject($subject)
+  {
+    $this->mailer->Subject = $subject;
+  }
+
+  public function body($body)
+  {
+    $this->mailer->Body = $body;
+  }
+
+}
